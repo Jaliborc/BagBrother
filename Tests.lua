8ee6f25bdd2098ed5704e03b207a52f2a30b6101
Jaliborc <contact@jaliborc.com>
2014-02-18 15:54:40 +0000

    First commit

---
+++ Tests.lua
@@ -0,0 +1,27 @@
+local Tests = WoWUnit('BagBrother')
+local AreEqual, Replace = WoWUnit.AreEqual, WoWUnit.Replace
+
+function Tests:BasicItem()
+	local link = '|cffa335ee|Hitem:3246:0:0:0:0:0:0:0:0:0:0|h[Big Bunny]|h|r'
+
+	AreEqual('3246', BagBrother:ParseItem(link, 1))
+	AreEqual('3246;2', BagBrother:ParseItem(link, 2))
+end
+
+function Tests:ComplexItem()
+	local link = '|cffa335ee|Hitem:3246:0:5:3:0:0:0:0:0:0:0|h[Scary Sword]|h|r'
+	local expected = '3246:0:5:3:0:0:0:0:0:0:0'
+	
+	AreEqual(expected, BagBrother:ParseItem(link, 1))
+end
+
+function Tests:CagedPet()
+	local link = '|cffa335ee|Hbattlepet:236:1:2:157:10:10:0x000000000000|h[Mountain Rat]|h|r'
+	local expected = '236:1:2:157:10:10:0'
+
+	AreEqual(expected, BagBrother:ParseItem(link, 1))
+end
+
+function Tests:NoItem()
+	AreEqual(nil, BagBrother:ParseItem(nil, nil))
+end
