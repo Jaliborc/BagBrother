c37990acb9ce38d7e4e50e923be39ef8922da955
semecan <semecan>
2020-05-09 15:01:55 +0200

    Condense bag and bank slots for item counts

--- ItemCount.lua
+++ ItemCount.lua
@@ -18,7 +18,40 @@ This file is part of BagBrother.
 local _, addon = ...
 local BagBrother = addon.BagBrother
 
+local FIRST_BAG_SLOT = BACKPACK_CONTAINER
+local LAST_BAG_SLOT = FIRST_BAG_SLOT + NUM_BAG_SLOTS
+local LAST_BANK_SLOT = NUM_BAG_SLOTS + NUM_BANKBAGSLOTS
+local FIRST_BANK_SLOT = NUM_BAG_SLOTS + 1
+local BAG_TYPE_BANK = 'bank'
+local BAG_TYPE_BAG = 'bags'
+
 local itemCountCache = {}
+
+local function getBagType (bag)
+  if (type(bag) ~= 'number') then
+    return bag
+  end
+
+  if (bag >= FIRST_BAG_SLOT and bag <= LAST_BAG_SLOT) then
+    return BAG_TYPE_BAG
+  end
+
+  if (bag == BANK_CONTAINER) then
+    return BAG_TYPE_BANK
+  end
+
+  if (bag >= FIRST_BANK_SLOT and bag <= LAST_BANK_SLOT) then
+    return BAG_TYPE_BANK
+  end
+
+  if (REAGENTBANK_CONTAINER ~= nil and bag == REAGENTBANK_CONTAINER) then
+    return BAG_TYPE_BANK
+  end
+
+  -- this part should never be reached
+  -- print('unknown bag:', bag)
+  return BAG_TYPE_BAG
+end
 
 function addon:InitItemCounts ()
   local BrotherBags = _G.BrotherBags or {}
@@ -36,11 +69,13 @@ function addon:InitItemCounts ()
       realmCounts[owner] = ownerCounts
 
       for bag, bagData in pairs (ownerData) do
-        local bagCounts = {}
-
-        ownerCounts[bag] = bagCounts
-
         if (type(bagData) == 'table') then
+          local bagCounts
+
+          bag = getBagType(bag)
+          ownerCounts[bag] = ownerCounts[bag] or {}
+          bagCounts = ownerCounts[bag]
+
           for slot, item in pairs(bagData) do
             if (type(slot) == 'number' and type(item) == 'string') then
               local link, count = strsplit(';', item)
@@ -48,6 +83,7 @@ function addon:InitItemCounts ()
 
               id = tonumber(id)
               count = count or 1
+              count = tonumber(count)
 
               if (bagCounts[id] == nil) then
                 bagCounts[id] = count
@@ -64,12 +100,12 @@ end
 
 function addon:GetItemCount (realm, owner, bag, itemId)
   local data = itemCountCache[realm]
-  if (data == nil) then return 0 end
+  if (data == nil) then return end
   data = data[owner]
-  if (data == nil) then return 0 end
+  if (data == nil) then return end
   data = data[bag]
-  if (data == nil) then return 0 end
+  if (data == nil) then return end
   data = data[itemId]
-  if (data == nil) then return 0 end
+  if (data == nil) then return end
   return data
 end
